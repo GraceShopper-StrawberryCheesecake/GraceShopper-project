1b9e4d3c741c0ad0105b762a8a95ce90fa6ffb7a
Kyle Combs <kylecombs429@gmail.com>
2021-08-04 10:56:45 -0500

    closes #3

--- client/components/SingleItem.js
+++ client/components/SingleItem.js
@@ -14,17 +14,14 @@ class SingleItem extends React.Component {
     const item = this.props.item;
     return (
       <div className="single-item-div">
-        <img src="https://sugarspunrun.com/wp-content/uploads/2019/01/Best-Cheesecake-Recipe-2-1-of-1-7.jpg" />
+        <img src={item.imgUrl} />
         <div className="single-item-info">
           <h1>{item.name}</h1>
           <p id="item-description">
-            {item.description}Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Donec consectetur velit in elit
-            porttitor, et tincidunt mi maximus. Integer ac lorem quis eros
-            facilisis semper in id nunc.
+            {item.description}
           </p>
           <div id="price-and-button">
-          <p> price:  $10{item.price}</p>
+          <p> price: $ {item.price}</p>
           <Button variant="outlined">Add to cart</Button>
           </div>
         </div>
