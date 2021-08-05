d265e4132b87a538b57f6c6affc2b218e31b9ef8
James Goering <jamesgoering@Jamess-MacBook-Pro.local>
2021-08-05 11:39:01 -0400

    closes #8

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -5,18 +5,48 @@ import { fetchOrder } from "../store/order"
 
 class Cart extends React.Component {
 
+    constructor() {
+        super()
+        this.state = {
+            order: {}
+        }
+    }
+
+    // when the component mounts we check if the user is logged in and get the order rom the database
+    // otherwise, we pull from window.localStorage and setState 
     componentDidMount () {
-        this.props.getOrder(this.props.customerId)
+        if (this.props.customerId) {
+            this.props.getOrder(this.props.customerId)
+        } else {
+            const order = JSON.parse(window.localStorage.getItem('order'))
+            if (order) {
+                this.setState({
+                    order
+                })
+            }
+        }
+    }
+
+    // after the database call, we set the state with the user's order
+    componentDidUpdate() {
+        if (!this.state.order.id && this.props.order) {
+            this.setState({
+                order: this.props.order
+            })
+        }
     }
 
     render () {
         return (
             <div>
                 <h1>CART</h1>
-                {this.props.order.items && this.props.order.items.map((item, index) => <div key={index} >{item.name}</div>)}
+                {this.state.order.items && this.state.order.items.map((item, index) => <div key={index} >{item.name}</div>)}
+                {/* {this.props.order.items && window.localStorage.setItem('order', JSON.stringify(this.props.order))} */}
+            
             </div>
         )
     }
+
 }
 
 const mapState = (state) => ({
